20dcb2a029db451ea1680c02b4e3020a6ea5b22d
gianfrancoscapin <gianscapin96@gmail.com>
2022-08-12 00:49:42 -0300

    add shared preferences and stateflow

--- app/src/main/java/com/gscapin/blogger/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/gscapin/blogger/presentation/home/HomeViewModel.kt
@@ -2,10 +2,16 @@ package com.gscapin.blogger.presentation.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.gscapin.blogger.core.Result
+import com.gscapin.blogger.data.model.Post
 import com.gscapin.blogger.domain.home.HomeRepoImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -32,4 +38,19 @@ class HomeViewModel @Inject constructor(private val repository: HomeRepoImpl): V
             emit(Result.Failure(e))
         }
     }
+
+    val seeNewPosts: StateFlow<Result<List<Post>>> = flow {
+        emit(Result.Loading())
+        kotlin.runCatching {
+            repository.getLastestPosts()
+        }.onSuccess { posts ->
+            emit(posts)
+        }.onFailure {
+            emit(Result.Failure(Exception(it.message)))
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = Result.Loading()
+    )
 }
--- app/src/main/java/com/gscapin/blogger/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gscapin/blogger/ui/home/HomeFragment.kt
@@ -1,15 +1,17 @@
 package com.gscapin.blogger.ui.home
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.gscapin.blogger.R
@@ -28,43 +30,83 @@ class HomeFragment : Fragment(R.layout.fragment_home), OnPostClickListener {
     private lateinit var binding: FragmentHomeBinding
     val viewModel: HomeViewModel by viewModels()
 
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        val sharedPreference =  activity?.getSharedPreferences("POSTS_BLOG", Context.MODE_PRIVATE)
+        var edit = sharedPreference?.edit()
 
         binding = FragmentHomeBinding.bind(view)
 
         viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.getLastestPosts().observe(viewLifecycleOwner, Observer { result ->
-                when (result) {
-                    is Result.Loading -> {
-                        binding.progressBar.visibility = View.VISIBLE
-                    }
-                    is Result.Success -> {
-                        binding.progressBar.visibility = View.GONE
-                        if (result.data.isEmpty()) {
-                            binding.postsEmpty.visibility = View.VISIBLE
-                            return@Observer
-                        } else {
-                            binding.postsEmpty.visibility = View.GONE
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                getPosts(edit)
+                viewModel.seeNewPosts.collect{result ->
+                    when(result){
+                        is Result.Loading -> {}
+                        is Result.Success -> {
+                            // usar shared preferences
+                            val listPosts = getPostsSP(sharedPreference)
+                            if(result.data.toString() != listPosts!!){
+                                binding.refreshPosts.show()
+                            }
                         }
-                        binding.rvHome.adapter = HomeAdapter(result.data, this@HomeFragment)
-                    }
-                    is Result.Failure -> {
-                        binding.progressBar.visibility = View.GONE
-                        Toast.makeText(
-                            requireContext(),
-                            "Error ${result.exception}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        Log.d("Error posts", "${result.exception}")
+                        is Result.Failure -> {
+                            Toast.makeText(
+                                requireContext(),
+                                "Error ${result.exception}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 }
-            })
+            }
         }
 
+
+
         logOut()
+        actionRefreshPosts(edit)
+    }
+
+    private fun actionRefreshPosts(edit: SharedPreferences.Editor?) {
+        binding.refreshPosts.setOnClickListener {
+            getPosts(edit)
+            binding.refreshPosts.hide()
+        }
+    }
+
+    private fun getPosts(edit: SharedPreferences.Editor?) {
+        viewModel.getLastestPosts().observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Result.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+                is Result.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    if (result.data.isEmpty()) {
+                        binding.postsEmpty.visibility = View.VISIBLE
+                        return@Observer
+                    } else {
+                        binding.postsEmpty.visibility = View.GONE
+                    }
+                    binding.rvHome.adapter = HomeAdapter(result.data, this@HomeFragment)
+                    edit?.putString("postSP",result.data.toString())
+                    edit?.commit()
+                }
+                is Result.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(
+                        requireContext(),
+                        "Error ${result.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.d("Error posts", "${result.exception}")
+                }
+            }
+        })
     }
 
     private fun logOut() {
@@ -72,6 +114,11 @@ class HomeFragment : Fragment(R.layout.fragment_home), OnPostClickListener {
             FirebaseAuth.getInstance().signOut()
             findNavController().navigate(R.id.action_homeFragment_to_welcomeFragment)
         }
+    }
+
+    private fun getPostsSP(sharedPreference: SharedPreferences?): String? {
+        val posts = sharedPreference?.getString("postSP","")
+        return posts;
     }
 
 
